443fe83ebab45ab5c8324f1229b230998b0f310d
nouri <nouri@DESKTOP-JUVMHBV>
2020-08-27 11:02:18 -0400

    Full proofed analysis

--- statsAnalysis.py
+++ statsAnalysis.py
@@ -2,20 +2,38 @@ from statistics import *
 import os
 import csv
 
+
 def outputStatistics(data: list, metric: str, outputDir: str):
+
+    # validate
+    if len(data) < 1:
+        return
+
+    # calculate and output
+    stats = calculateStats(data)
+
+    # output
+    with open(os.path.join(outputDir, "project.csv"), "a", newline="") as f:
+        w = csv.writer(f, delimiter=",")
+
+        for key in stats:
+            outputValue(w, metric, key, stats)
+
+
+def calculateStats(data):
 
     # get non-zero data following the (+1 data / -1 result) approach
     nonZeroData = [value + 1 for value in data]
 
-    # calculate and output
-    with open(os.path.join(outputDir, "project.csv"), "a", newline="") as f:
-        w = csv.writer(f, delimiter=",")
-
+    stats = dict(
         
-        outputValue(w, metric, "pstdev", pstdev(data))
-        
+        stdev=stdev(data) if len(data) > 1 else None,
+    )
 
-def outputValue(w, metric: str, name: str, value, reduceByOne=False):
+    return stats
+
+
+def outputValue(w, metric: str, name: str, dict: dict):
+    value = dict[name]
     name = "{0}_{1}".format(metric, name)
-    value = value - 1 if reduceByOne else value
     w.writerow([name, value])
