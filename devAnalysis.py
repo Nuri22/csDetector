b060242367135d811b16882b2e2f4b35b69049ce
nouri <nouri@DESKTOP-JUVMHBV>
2020-09-13 20:41:53 -0400

    Completed dev analysis functionality

--- devAnalysis.py
+++ devAnalysis.py
@@ -1,19 +1,15 @@
 import os
 import csv
 
-def devAnalysis(authorInfoDict: set, issueOrPrDevs: set, outputDir: str):
+def devAnalysis(authorInfoDict: set, batchIdx: int, devs: set, outputDir: str):
     
     # select experienced developers
     experiencedDevs = [login for login, author in authorInfoDict.items() if author['experienced'] == True]
 
     # filter by developers present in list of aliased developers by commit
-    numberActiveExperiencedDevs = len(issueOrPrDevs.intersection(set(experiencedDevs)))
+    numberActiveExperiencedDevs = len(devs.intersection(set(experiencedDevs)))
     
     print("Writing active experienced developer analysis results")
-    with open(os.path.join(outputDir, "project.csv"), "a", newline="") as f:
+    with open(os.path.join(outputDir, f"project_{batchIdx}.csv"), "a", newline="") as f:
         w = csv.writer(f, delimiter=",")
         w.writerow(["NumberActiveExperiencedDevs", numberActiveExperiencedDevs])
-
-def splitRepoName(repoShortName: str):
-    split = repoShortName.split("/")
-    return split[0], split[1]
