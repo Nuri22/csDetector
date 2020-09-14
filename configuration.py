6a4d14a948de71de0e11a10233b2705130695f71
nouri <nouri@DESKTOP-JUVMHBV>
2020-09-13 20:34:05 -0400

    Finished config file parameters

--- configuration.py
+++ configuration.py
@@ -1,8 +1,22 @@
 class Configuration:
-    def __init__(self, repositoryUrl: str, repositoryPath: str, repositoryShortname: str, analysisOutputPath: str, aliasPath:str, aliasSimilarityMaxDistance: float):
+    def __init__(
+        self,
+        repositoryUrl: str,
+        repositoryPath: str,
+        batchSizeInMonths: int,
+        repositoryShortname: str,
+        analysisOutputPath: str,
+        aliasPath: str,
+        aliasSimilarityMaxDistance: float,
+        sentiStrengthJarPath: str,
+        sentiStrengthDataPath: str,
+    ):
         self.repositoryUrl = repositoryUrl
         self.repositoryShortname = repositoryShortname
         self.repositoryPath = repositoryPath
+        self.batchSizeInMonths = batchSizeInMonths
         self.analysisOutputPath = analysisOutputPath
         self.aliasPath = aliasPath
         self.aliasSimilarityMaxDistance = aliasSimilarityMaxDistance
+        self.sentiStrengthJarPath = sentiStrengthJarPath
+        self.sentiStrengthDataPath = sentiStrengthDataPath
